fc25e3ff9d850f81aaf27ec5b99a905c293ff2f2
Tairox <hds.anthony@gmail.com>
2022-09-22 23:33:35 +0200

    Hébergement SQL zippé sur domaine hdesousa.fr

--- deZipFromWeb.py
+++ deZipFromWeb.py
@@ -2,12 +2,13 @@ import requests,zipfile,io,filecmp
 from datetime import date
 from ourFunctions import formatDate,tgzMyFile
 
-r = requests.get("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-zip-file.zip", stream=True)
+filename="Sample-SQL-File-10-Rows.sql"
+r = requests.get("https://hdesousa.fr/downloads/Sample-SQL-File-10-Rows.sql.zip", stream=True)
 if r.ok:
     print("L'URL de téléchargement existe.")
     z = zipfile.ZipFile(io.BytesIO(r.content))
     try:
-        z.extract("sample.txt",".") # sample.txt est le nom du fichier attendu dans le zip
+        z.extract(filename,".") # sample.txt est le nom du fichier attendu dans le zip
         print("Le fichier .zip contient bien le fichier attendu.")
     except KeyError:
         print("Le fichier .zip ne contient pas le fichier attendu")
@@ -22,9 +23,9 @@ yearnumber=today.year
 
 finalDate=formatDate(daynumber,monthnumber,yearnumber)
 
-tgzMyFile(finalDate,"sample.txt")
+tgzMyFile(finalDate,filename)
 
-if filecmp.cmp("sample.txt","monfichierdiff.txt"): # si les fichiers sont identiques
+if filecmp.cmp(filename,"monfichierdiff.sql"): # si les fichiers sont identiques
     print("Erreur : Le fichier est le même que celui de la veille")
 else:
     print("Le fichier n'est pas le même que celui de la veille")
