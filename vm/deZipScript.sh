a3e7c7855ffda55480674581a3bef3d90d32bf8c
ArweeZ <114164237+ArweeZ@users.noreply.github.com>
2022-10-03 18:26:19 +0200

    j'ai enlever les logs

--- vm/deZipScript.sh
+++ vm/deZipScript.sh
@@ -5,6 +5,6 @@ echo "Lancement du script"
 apt-get update  
 apt-get install python3 -y
 apt-get install python3-pip -y 
-python3 deZipFromWeb.py >> ~/bashlog.txt
+python3 deZipFromWeb.py 
 
 echo "Fin du script"
