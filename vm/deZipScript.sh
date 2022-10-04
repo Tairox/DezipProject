7ff68b9e174b9773f5c017b0a340885219aa1932
ArweeZ <114164237+ArweeZ@users.noreply.github.com>
2022-10-04 16:09:10 +0200

    pull de git

--- vm/deZipScript.sh
+++ vm/deZipScript.sh
@@ -2,10 +2,19 @@
 
 echo "Lancement du script"
 
-apt-get update  
+apt-get update # verification des update
+apt-get install git -y
 apt-get install python3 -y
 apt-get install python3-pip -y 
-pip install paramiko
-python3 deZipFromWeb.py 
+apt-get install cron -y
+git clone lien ~/ScriptingSystem # recuperation du projet github
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "*/1 * * * * ~/ScriptingSystem" >> mycron
+#install new cron file
+crontab mycron
 
 echo "Fin du script"
+rm mycron
