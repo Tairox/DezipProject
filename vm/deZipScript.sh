e6c78c533cb40c54dc0a7fc2f304abc33e733f02
Matteofirrone <firrone.matteo@gmail.com>
2022-10-08 17:54:20 +0200

    install

--- vm/deZipScript.sh
+++ vm/deZipScript.sh
@@ -2,21 +2,28 @@
 
 echo "Launching the script"
 
-sudo apt-get update  
-sudo apt-get install git -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y 
-sudo pip install paramiko
-sudo apt-get install cron -y
-sudo git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ];then
+    echo "Please run this script as root"
+    exit 1
+fi
+
+apt-get update  
+apt-get install git -y
+apt-get install python3 -y
+apt-get install python3-pip -y 
+pip install paramiko
+apt-get install cron -y
+git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
 
 # Copy the content of current crontab file to another temporary file mycron
-sudo crontab -l > mycron
+crontab -l > mycron
 # Add new cronjob to the temporary file
-sudo echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
+echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
 # We copy the temporary file back to crontab
-sudo crontab mycron
+crontab mycron
+
+# Deletion of the temporary file
+rm mycron
 
 echo "End of the script"
-# Deletion of the temporary file
-sudo rm mycron
