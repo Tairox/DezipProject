f2d7158fc53543ef40bb279d15f3acf42ca0431e
ArweeZ <114164237+ArweeZ@users.noreply.github.com>
2022-10-08 20:41:15 +0200

    reglage de l'erreur sur l'existance du directory

--- vm/deZipScript.sh
+++ vm/deZipScript.sh
@@ -3,27 +3,37 @@
 echo "Launching the script"
 
 # Check if the script is run as root
-if [ "$EUID" -ne 0 ];then
+if [ "$EUID" -ne 0 ]
+then
     echo "Please run this script as root"
-    exit 1
+else
+	# Check for update and install packages
+    	apt-get update  
+	apt-get install git -y
+	apt-get install python3 -y
+	apt-get install python3-pip -y 
+	pip install paramiko
+	apt-get install cron -y
+	
+	# Check if the directory exists
+	DIR=~/ScriptingSystem
+	if [ -d "$DIR" ];
+	then
+   		rm -r ~/ScriptingSystem
+	fi
+	git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
+
+	# Copy the content of current crontab file to another temporary file mycron
+	crontab -l > mycron
+	# Add new cronjob to the temporary file
+	echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
+	# We copy the temporary file back to crontab
+	crontab mycron
+	
+	# Deletion of the temporary file
+	rm mycron
 fi
 
-apt-get update  
-apt-get install git -y
-apt-get install python3 -y
-apt-get install python3-pip -y 
-pip install paramiko
-apt-get install cron -y
-git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
 
-# Copy the content of current crontab file to another temporary file mycron
-crontab -l > mycron
-# Add new cronjob to the temporary file
-echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
-# We copy the temporary file back to crontab
-crontab mycron
-
-# Deletion of the temporary file
-rm mycron
 
 echo "End of the script"
