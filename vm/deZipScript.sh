62703d31401090f6d8e796e72da62c51af7d37cb
Matteofirrone <firrone.matteo@gmail.com>
2022-10-08 10:29:12 +0200

    sudo

--- vm/deZipScript.sh
+++ vm/deZipScript.sh
@@ -2,21 +2,21 @@
 
 echo "Launching the script"
 
-apt-get update  
-apt-get install git -y
-apt-get install python3 -y
-apt-get install python3-pip -y 
-pip install paramiko
-apt-get install cron -y
-git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
+sudo apt-get update  
+sudo apt-get install git -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y 
+sudo pip install paramiko
+sudo apt-get install cron -y
+sudo git clone https://github.com/Tairox/DezipProject ~/ScriptingSystem
 
 # Copy the content of current crontab file to another temporary file mycron
-crontab -l > mycron
+sudo crontab -l > mycron
 # Add new cronjob to the temporary file
-echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
+sudo echo "0 8 * * * python3 ~/ScriptingSystem/deZipFromWeb.py" >> mycron
 # We copy the temporary file back to crontab
-crontab mycron
+sudo crontab mycron
 
 echo "End of the script"
 # Deletion of the temporary file
-rm mycron
+sudo rm mycron
